7ac8a6efec79de120278694d10f9f821aff63d76
wyp-blizzmi <wuyp@blizzmi.com>
2017-09-21 15:10:29 +0800

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule Exjieba.Mixfile do
       version: "0.0.2",
       elixir: ">= 1.0.0",
       compilers: [ :segment, :elixir, :app ],
-      deps: deps
+      deps: deps()
     ]
   end
 
